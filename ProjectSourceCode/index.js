af46c7101d75b0d63f56feddd6fde8d0e097ff54
Estella Duong <esdu4090@colorado.edu>
2025-04-03 11:50:56 -0600

    basic setup + some images added + navbar

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -5,12 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Set up Handlebars as the view engine
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views')); 
 
-// Serve static files (CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
