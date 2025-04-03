64f375b09f66ce4291302aa17ad7fac6d346e80b
Carolyn Dockhorn <carolyn.dockhorn@colorado.edu>
2025-04-03 12:44:01 -0600

    general changes branch

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -7,15 +7,9 @@ const PORT = 3000;
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Start the server
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
-
 
 // *****************************************************
 //                    API Routes
@@ -24,3 +18,10 @@ app.listen(PORT, '0.0.0.0', () => {
 app.get('/', (req, res) => {
     res.render('pages/home');
 });
+
+app.get('/welcome', (req, res) => {
+    res.json({ status: 'success', message: 'Welcome!' });
+});
+
+module.exports = app.listen(3000);
+console.log("Server running");
