cc07b817b0eb56588267c4dff12b6af6ff14f426
Carolyn Dockhorn <carolyn.dockhorn@colorado.edu>
2025-04-08 01:54:20 -0600

    about us basic layout

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,8 +20,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/welcome', (req, res) => {
-    res.json({status: 'success', message: 'Welcome!'});
-  });
+    res.json({ status: 'success', message: 'Welcome!' });
+});
 
 module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
+
+
+// FRIENDS PAGE
+app.get('/friends', (req, res) => {
+    res.render('pages/friends')
+
+});
+
+// ABOUT US PAGE
+app.get('/aboutus', (req, res) => {
+    res.render('pages/aboutus')
+
+});
